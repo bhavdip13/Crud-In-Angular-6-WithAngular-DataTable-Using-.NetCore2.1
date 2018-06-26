ab86d07df3c6968a422cb4e3663552b3f2b35911
talaviya bhavdip <talaviya.bhavdip@outlook.com>
2018-06-26 09:55:53 +0530

    after comit sql script file

--- CrudInAngular5With.NetCore2.1/ClientApp/src/app/app.module.ts
+++ CrudInAngular5With.NetCore2.1/ClientApp/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+
 import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { AddEmployeeComponent } from './Components/add-employee/add-employee.component';
+import { FetchEmployeeComponent } from './Components/fetch-employee/fetch-employee.component';
+import { EmpserviceService } from './empservice.service';
+
 
 @NgModule({
   declarations: [
@@ -16,19 +24,30 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    AddEmployeeComponent,
+    FetchEmployeeComponent
   ],
+  
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
+    HttpModule,
     FormsModule,
+    ReactiveFormsModule,
+
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'home', component: HomeComponent },
+      { path: 'fetch-employee', component: FetchEmployeeComponent },
+      { path: 'add-employee', component: AddEmployeeComponent },
+      { path: 'employee/edit/:id', component: AddEmployeeComponent },
+      { path: '**', redirectTo: 'home' }  
+
+
     ])
   ],
-  providers: [],
+  providers: [EmpserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
